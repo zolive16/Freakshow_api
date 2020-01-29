c2c1c5d88ecb8029ddf90c18eb099a997ca0464e
zolive16 <collignon_stephanie@yahoo.fr>
2020-01-29 21:24:06 +0100

    avancée des routes

--- routes/performance.js
+++ routes/performance.js
@@ -20,11 +20,37 @@ router.post("/new", (req, res) => {
     const formData = req.body;
     connection.query("INSERT INTO performance SET ?", formData, (err, result) => {
         if (err) {
-            res.status(500).send(err);
+            res.status(500).send("Erreur lors de la création d'une performance");
         } else {
             res.sendStatus(200).json(result);
         }
     });
 });
+
+//modification d'une performance
+router.put("/:id", (req, res) => {
+    const idUrl = req.params.id;
+    const formData = req.body;
+    connection.query("UPDATE performance SET ? WHERE id = ?", [formData, idUrl], err => {
+        if (err) {
+            res.status(500).send("Erreur lors de la modification d'une performance");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+//supprimer une performance
+router.delete("/:id",
+    (req, res) => {
+        const idUrl = req.params.id;
+        connection.query("DELETE FROM performance WHERE id = ?", [idUrl], err => {
+            if (err) {
+                res.status(500).send("Erreur lors de la suppression d'une performance");
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    });
 
 module.exports = router;

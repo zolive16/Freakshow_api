79f5b4f6f92fbac38b84a2e0828b06a9980bb386
zolive16 <collignon_stephanie@yahoo.fr>
2020-01-29 19:13:00 +0100

    post crée + modification .env

--- routes/performance.js
+++ routes/performance.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const connection = require("../config");
 const router = express.Router({
-	mergeParams: true
+    mergeParams: true
 });
 
+//toutes les performances
 router.get("/", (req, res) => {
-	connection.query("SELECT * FROM performance", (err, results) => {
-		if (err) {
-			res.status(500);
-		} else {
-			res.json(results);
-		}
-	});
+    connection.query("SELECT * FROM performance", (err, results) => {
+        if (err) {
+            res.status(500);
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+//création d'une nouvelle performance
+router.post("/new", (req, res) => {
+    const formData = req.body;
+    connection.query("INSERT INTO performance SET ?", formData, (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.sendStatus(200).json(result);
+        }
+    });
 });
 
 module.exports = router;
